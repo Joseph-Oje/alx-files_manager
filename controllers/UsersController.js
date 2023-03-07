c7ebd370ab88290efde49336e9a54d00afe2d39f
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-03-07 20:29:53 +0300

    remove unwanted file

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,40 +1,32 @@
-const sha1 = require('sha1');
-const User = require('../utils/User');
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
-exports.postNew = async (req, res) => {
-    // Get the email and password from the request body
-    const { email, password } = req.body;
+export default class UsersController {
+    static postNew(req, res) {
+        const email = `${req.body && req.body.email ? req.body.email : ''}`;
+        const password = `${req.body && req.body.password ? req.body.password : ''}`;
 
-    // Check if the email and password are present in the request body
-    if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
+        if (email.trim().length === 0) {
+            res.status(400).json({ error: 'Missing email' });
+            return;
+        }
+        if (password.trim().length === 0) {
+            res.status(400).json({ error: 'Missing password' });
+            return;
+        }
+        dbClient.usersCollection()
+            .then(async (usersCollection) => {
+                const tmpUser = await usersCollection.findOne({ email });
+
+                if (tmpUser) {
+                    res.status(400).json({ error: 'Already exist' });
+                    return;
+                }
+                const user = await usersCollection.insertOne({
+                    email,
+                    password: sha1(password),
+                });
+                res.status(200).json({ email, id: user.insertedId.toString() });
+            });
     }
-    if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-    }
-
-    // Check if the email already exists in the database
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        return res.status(400).json({ error: 'Already exist' });
-    }
-
-    // Hash the password using SHA1
-    const hashedPassword = sha1(password);
-
-    // Create a new user object with the email and hashed password
-    const newUser = new User({
-        email,
-        password: hashedPassword,
-    });
-
-    // Save the new user to the database
-    try {
-        await newUser.save();
-        // Return the new user with only the email and id fields and a status code of 201
-        res.status(201).json({ email: newUser.email, id: newUser._id });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-};
+}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 // Import the controllers
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 // Create the router
 const router = express.Router();
@@ -11,7 +12,9 @@ const router = express.Router();
 // Define the routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getStats);
+router.get('/users/me ', UsersController.getStats);
 // Define the route for creating a new user
 router.post('/users', UsersController.postNew);
 
